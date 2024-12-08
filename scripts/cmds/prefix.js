503d5c31802a1bda845f837e59a283598fc8764e
Lord9s <nkunajunior399@gmail.com>
2024-12-08 14:54:52 +0200

    Update prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -2,7 +2,7 @@ module.exports = {
  config: {
 	 name: "prefix",
 	 version: "1.0",
-	 author: "ʭɸʭ Sønïč Shïsûį ʭɸʭ",// don't change this credit
+	 author: "Lord king",// don't change this credit
 	 countDown: 5,
 	 role: 0,
 	 shortDescription: "no prefix",
@@ -14,7 +14,7 @@ module.exports = {
  onChat: async function({ event, message, getLang }) {
  if (event.body && event.body.toLowerCase() === "prefix") {
  return message.reply({
- body: `🎯𝐏𝐑𝐄𝐅𝐈𝐗👻𝐒𝐇𝐈𝐒𝐔𝐈🎯:↠%1↞\n━━━━━━━━━━━━━━━━\n🖤𝐔𝐂𝐇𝐈𝐖𝐀👽𝐒𝐘𝐒𝐓𝐄𝐌🖤:⇨%2⇦\n━━━━━━━━━━━━━━━━\n🏁☘𝑀𝑌 𝐶𝑅𝐸𝐴𝑇𝑂𝑅☘🏁\n━━━━━━━━━━━━━━━━\n🥷ʬɸʬ Sønïč❤‍🔥Shïsûį ʬɸʬ🥷\n━━━━━━━━━━━━━━━━\n👷✨𝗧𝗮𝗽𝗲 %1𝗵𝗲𝗹𝗽 𝗽𝗼𝘂𝗿 𝗮𝘃𝗼𝗶𝗿 𝗹𝗮 𝗹𝗶𝘀𝘁𝗲 𝗱𝗲 𝗰𝗼𝗺𝗺𝗮𝗻𝗱𝘀✨👷`,
+ body: `🎯𝐏𝐑𝐄𝐅𝐈𝐗👻𝐒𝐇𝐈𝐒𝐔𝐈🎯:↠%1↞\n●▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬●\n🖤𝐔𝐂𝐇𝐈𝐖𝐀👽𝐒𝐘𝐒𝐓𝐄𝐌🖤:⇨%2⇦\n●▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬●\n🏁☘𝑀𝑌 𝐶𝑅𝐸𝐴𝑇𝑂𝑅☘🏁\n●▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬●\n🥷𝐔𝐂𝐇𝐈𝐖𝐀 𝐅𝐀𝐌𝐈𝐋𝐘🥷\n●▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬●\n👷✨𝘵𝘺𝘱𝘦 %1𝘩𝘦𝘭𝘱 𝘵𝘰 𝘴𝘦𝘦 𝘢𝘭𝘭 𝘮𝘺 𝘤𝘰𝘮𝘮𝘢𝘯𝘥𝘴✨👷`,
  attachment: await global.utils.getStreamFromURL("https://i.ibb.co/By078j8/image.gif")
  });
  }
